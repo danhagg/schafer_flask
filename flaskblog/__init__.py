3e1561902131868ba3569717616c9a82afa5884b
danhagg <danielhaggerty1976@gmail.com>
2018-08-23 08:11:14 -0500

    moved secret keys to environment variables

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,16 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-import os
+from flaskblog.config import Config
 
 # "__name__" name of module
 app = Flask(__name__)
-
-# Must be made an environment variable later on
-app.config['SECRET_KEY'] = '11713771a462f1248420575583db66db'
-# "///" relative path from current file
-# Will change it to postgresql at production
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 
 # Creat db instance
 db = SQLAlchemy(app)
@@ -21,12 +16,6 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-# Environment Variables in .bash_profile
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
 mail = Mail(app)
 
 # import blueprints and register
